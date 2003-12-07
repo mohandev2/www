175c0526d691063d59cccbf588660677e4f75d0e
sdague <sdague@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2003-12-07 02:00:04 +0000

    new extract items that can pull from the trackers

---
+++ htdocs/tracker/arts2html.pl
@@ -0,0 +1,103 @@
+#!/usr/bin/perl
+
+use strict;
+use Data::Dumper;
+
+our $VAR1;
+
+my @RELEASES = ("0.3","0.4","0.5","0.6","0.7","0.8","0.9","1.0","Future","None");
+
+my @f = load_items("features.txt");
+my @b = load_items("bugs.txt");
+
+my $features = {};
+foreach my $tmp (@f) {
+    $tmp->{Type} = "Feature";
+    if($tmp->{Status} =~ /Deleted/) {
+        next;
+    }
+    # probably old
+    if($tmp->{Status} =~ /Closed/ and
+       $tmp->{Group} =~ /None/) {
+        next;
+    }
+    
+    $features->{$tmp->{Group}}->{$tmp->{Number}} = $tmp;
+    
+}
+
+my $bugs = {};
+foreach my $tmp (@b) {
+    $tmp->{Type} = "Bug";
+    if($tmp->{Status} =~ /Deleted/) {
+        next;
+    }
+    # probably old
+    if($tmp->{Status} =~ /Closed/ and
+       $tmp->{Group} =~ /None/) {
+        next;
+    }
+    $bugs->{$tmp->{Group}}->{$tmp->{Number}} = $tmp;
+}
+
+foreach my $rel (@RELEASES) {
+    if(exists $bugs->{$rel} or exists $features->{$rel}) { 
+        print "<h2>Release Status for $rel</h2>\n";
+        print "<table>\n";
+
+        print "<tr><th colspan=5>Features</th></tr>\n";
+        foreach my $bid (sort keys %{$features->{$rel}}) {
+            my $bug = $features->{$rel}->{$bid};
+            print_item_html($bug);
+        }
+
+        print "<tr><th colspan=5>Bugs</th></tr>\n";
+        foreach my $bid (sort keys %{$bugs->{$rel}}) {
+            my $bug = $bugs->{$rel}->{$bid};
+            print_item_html($bug);
+        }
+        print "</table>\n";
+    }
+}
+
+sub print_item_html {
+    my $art = shift;
+    my $bid = $art->{Number};
+    my $status = $art->{Status};
+    my $flag = $status;
+    my $owner = $art->{"Assigned To"};
+    my $cat = $art->{Category};
+    my $resolve = $art->{Resolution};
+    my $sum = $art->{Title};
+
+    if($owner =~ /Nobody/ and $status !~ /Closed/) {
+        $flag = "Bad";
+    }
+    
+    my $url = "https://sourceforge.net/tracker/index.php?func=detail&amp;aid=$bid&amp;group_id=71730";
+    if($art->{Type} =~ /Feature/) {
+        $url .= "&amp;atid=532254";
+    } elsif($art->{Type} =~ /Bug/) {
+        $url .= "&amp;atid=532251";
+    }
+    
+    print "<tr>\n<td class=$flag>$bid</td>\n";
+    print "<td class=sumary><a href=\"$url\">$sum</a></td><td class=owner>$owner</td>\n";
+    print "<td class=status>$status - $resolve</td></tr>\n";
+    
+}
+
+sub load_items {
+    $VAR1 = undef;
+    my $file = shift;
+    local $/ = undef;
+    open(IN,"<$file");
+    my $var = <IN>;
+    close(IN);
+    
+    eval($var);
+
+    my @value = @$VAR1;
+    
+    return @value;
+}
---
+++ htdocs/tracker/sfexport.pl
@@ -0,0 +1,121 @@
+#!/usr/bin/perl
+#
+#  Perl program to pull everything out of the trackers and build a artifact
+#  text files in the process.  Use other programs in this directory to do 
+#  useful things with these pulls
+#
+#  Note: if SF gave access to their export anonymously, or let agents authenticate
+#  with the site, we could do this in 2 requests instead of the couple hundred
+#  that is currently required.
+#
+#  (C) Copyright 2003 IBM Corp.
+#
+#  Author: Sean Dague
+#
+#  This file is distributed under that same terms as OpenHPI itself
+
+use LWP::UserAgent;
+use HTML::LinkExtor;
+use HTTP::Response;
+use HTTP::Request::Common;
+use Data::Dumper;
+#use LWP::Debug qw(level); level('+');
+use strict;
+
+our @track = ();
+
+my $ua = new LWP::UserAgent();
+
+my @bugs = process_tracker(532251);
+my @features = process_tracker(532254);
+
+open(OUT,">bugs.txt");
+print OUT Dumper(\@bugs);
+close(OUT);
+
+open(OUT,">features.txt");
+print OUT Dumper(\@features);
+close(OUT);
+
+#http://sourceforge.net/tracker/index.php?func=browse&group_id=71730&atid=532251&set=custom&_assigned_to=0&_status=100&_category=100&_group=100&order=artifact_id&sort=ASC&offset=50
+
+#print $res->as_string;
+
+sub callback {
+    my($tag, %attr) = @_;
+    if($tag eq 'a' and $attr{href} =~ m{/tracker/index.php}) {
+        #        print Dumper(\%attr);
+        push @track, \%attr;
+    }
+}
+
+
+sub process_tracker {
+    my $tid = shift;
+    my @arts = ();
+    my @links = suck_tracker("http://sourceforge.net/tracker/index.php",
+                             group_id => 71730,
+                             atid => $tid,
+                             _status => 100,
+                             set => "custom",
+                            );
+ 
+    foreach my $i (@links) {
+        if($i->{href} =~ /offset/) { next; }
+
+        my %keys = get_artifact($i->{href});
+        print "processed $i->{href}\n";
+        push @arts, \%keys;
+    }
+    return @arts;
+}
+
+sub get_artifact {
+    my $relurl = shift;
+    my %keys = ();
+    my $res = $ua->request(GET "http://sourceforge.net" . $relurl);
+    
+    my $content = $res->content;
+    foreach my $key ("Category", "Group", "Assigned To", "Priority", "Status", "Resolution", "Date Closed") {
+        if($content =~ /($key):.*?<br>([^<]+)/s) {
+            $keys{$1} = $2;
+        }
+    }
+    if($content =~ /<title>SourceForge.net: Detail:(\d+) - (.*?)<\/title>/is) {
+        $keys{Number} = $1;
+        $keys{Title} = $2;
+    }
+    $keys{Link} = "http://sourceforge.net" . $relurl;
+    
+    return %keys;
+}
+
+sub suck_tracker {
+    my ($base, %vars) = @_;
+
+    # this has to be package scoped, other wise callbacks go crazy
+    @track = ();
+    
+    my $p = HTML::LinkExtor->new(\&callback);      
+    my $res = $ua->request(POST $base,
+                           \%vars);
+    
+    $p->parse($res->content);
+    
+    my $item = $track[scalar(@track) - 1];
+    my $offset = 0;
+    while($item->{href} =~ /offset=(\d+)/) {
+        my $newoff = $1;
+        if($newoff < $offset) {
+            last;
+        }
+        $offset = $newoff;
+        pop(@track);
+        $res = $ua->request(GET "http://sourceforge.net" . $item->{href});
+        $p->parse($res->content);
+        $item = $track[scalar(@track) - 1];
+    }
+    return @track;
+}
+
+    
