beda67494373d7da6e85c5d3c0ab0996f3e5a32c
renierm <renierm@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-10-12 16:43:26 +0000

    Made changes to people, contribute, tracker and status direcotries for using the new style.

--- htdocs/tracker/arts2html.pl
+++ htdocs/tracker/arts2html.pl
@@ -27,9 +27,9 @@ foreach my $i (sort keys %{$data->{features}->{data}}) {
        $tmp->{Group} =~ /None/) {
         next;
     }
-    
+
     $features->{$tmp->{Group}}->{$tmp->{Number}} = $tmp;
-    
+
 }
 
 my $bugs = {};
@@ -48,19 +48,19 @@ foreach my $i (sort keys %{$data->{bugs}->{data}}) {
 }
 
 foreach my $rel (@RELEASES) {
-    if(exists $bugs->{$rel} or exists $features->{$rel}) { 
-        $table .=  "<h2>Release Status for $rel</h2>\n";
-        $table .=  "<table>\n";
-        
+    if(exists $bugs->{$rel} or exists $features->{$rel}) {
+        $table .=  "<h4>Release Status for $rel</h4>\n";
+        $table .=  "<div><table>\n";
+
         print_group("Features",$features->{$rel});
         print_group("Bugs",$bugs->{$rel});
 
-        $table .= "</table>\n";
+        $table .= "</table></div>\n";
     }
 }
 
 ################
-# 
+#
 #  Now do file sub work
 #
 ################
@@ -71,7 +71,7 @@ foreach my $rel (@RELEASES) {
     close(IN);
 
     $content =~ s/%% data %%/$table/igs;
-    
+
     my $date = ctime(time());
 
     $content =~ s/%% time %%/$date/igs;
@@ -84,15 +84,15 @@ foreach my $rel (@RELEASES) {
 
 sub print_group {
     my ($name, $tracker) = @_;
-    $table .=  "<tr><th colspan=5>$name</th></tr>\n";
+    $table .=  "<tr><th colspan=\"5\">$name</th></tr>\n";
     my $group = "";
-    foreach my $id (sort 
+    foreach my $id (sort
                     {$tracker->{$a}->{Category}.$a cmp
                        $tracker->{$b}->{Category}.$b} keys %{$tracker}) {
-        
+
         if($group ne $tracker->{$id}->{Category}) {
             $group = $tracker->{$id}->{Category};
-            $table .=  "<tr><td class='group' colspan=5>$group</td></tr>\n";
+            $table .=  "<tr><td class=\"group\" colspan=\"5\">$group</td></tr>\n";
         }
         my $bug = $tracker->{$id};
         print_item_html($bug);
@@ -112,18 +112,18 @@ sub print_item_html {
     if($owner =~ /Nobody/ and $status !~ /Closed/) {
         $flag = "Bad";
     }
-    
+
     my $url = "https://sourceforge.net/tracker/index.php?func=detail&amp;aid=$bid&amp;group_id=71730";
     if($art->{Type} =~ /Feature/) {
         $url .= "&amp;atid=532254";
     } elsif($art->{Type} =~ /Bug/) {
         $url .= "&amp;atid=532251";
     }
-    
-    $table .= "<tr>\n<td class=$flag>$bid</td>\n";
-    $table .= "<td class=sumary><a href=\"$url\">$sum</a></td><td class=owner>$owner</td>\n";
-    $table .= "<td class=status>$status - $resolve</td></tr>\n";
-    
+
+    $table .= "<tr>\n<td class=\"$flag\">$bid</td>\n";
+    $table .= "<td class=\"summary\"><a href=\"$url\">$sum</a></td><td class=\"owner\">$owner</td>\n";
+    $table .= "<td class=\"status\">$status - $resolve</td></tr>\n";
+
 }
 
 #########################################
@@ -132,9 +132,9 @@ sub load_data {
     my $file = shift;
     my $VAR1 = undef;
     my $tmp;
-    
+
     return $data if(!-e $file);
-    
+
     open(IN,"<$file") or die "Can't open $file";
     {
         local $/ = undef;
