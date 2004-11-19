5466a05bf5dec66525dd197c743799027ae748ef
sdague <sdague@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-11-19 16:31:28 +0000

    limit what is pushed let BladeCenter work

--- www/trunk/htdocs/status/compile_status.pl
+++ www/trunk/htdocs/status/compile_status.pl
@@ -102,7 +102,7 @@ sub safename {
 }
 
 sub open_hpi_items {
-    my @array = ("OpenHPI Infrastructure","Dummy Plugin","IPMI Plugin","Sysfs Plugin","Snmp Client plugin","SNMP Blade Center", "Linux Watchdog Plugin");
+    my @array = ("OpenHPI Infrastructure","Dummy Plugin","IPMI Plugin","SNMP BladeCenter");
     return @array;
 }
 
