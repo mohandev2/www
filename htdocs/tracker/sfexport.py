72578c5b575e27793357dafe0f50016d5f6bd9b8
renierm <renierm@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2006-08-05 07:35:24 +0000

    Writing scripts to use SF xml export for generating changelog and tracker status.

---
+++ htdocs/tracker/sfexport.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+#
+# (C) Copyright IBM Corp. 2006
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. This
+# file and program are licensed under a BSD style license. See
+# the Copying file included with the OpenHPI distribution for
+# full licensing terms.
+#
+"""
+Fetch the sourceforge XML export file for the project
+
+Author(s):
+        Renier Morales <renierm@users.sf.net>
+"""
+import sys, os, time
+from optparse import OptionParser
+from getpass import getpass
+from mechanize import Browser
+
+optsparser = OptionParser()
+optsparser.add_option('-f',
+		      '--file',
+		      default='xml_export.xml',
+		      dest='xmlfile',
+		      help='Save the xml export to this file '
+			   '[default: %default]')
+options, args = optsparser.parse_args()
+
+# Get username and password
+username = ''
+if os.environ.has_key('SFUID'):
+	username = os.environ['SFUID']
+else:
+	username = raw_input('Enter your SourceForge username: ')
+
+password = getpass('Enter password for \'%s\': ' % username)
+
+# Login to sourceforge
+print 'Logging into SourceForge...'
+br = Browser()
+login_url = 'https://sourceforge.net/account/login.php'
+xml_url = 'https://sourceforge.net/export/xml_export.php?group_id=71730'
+br.open(login_url)
+br.select_form('login')
+br['form_loginname'] = username
+br['form_pw'] = password
+br.submit()
+time.sleep(1)
+
+# Download XML export file
+print 'Reading XML...'
+response = br.open(xml_url)
+xml = response.read()
+if len(xml) < 250:
+	print 'Unauthorized login or download corrupted. Nothing was saved.'
+	sys.exit(-1)
+	
+# Save file
+xmlfile = open(options.xmlfile, 'w')
+xmlfile.write(xml)
+xmlfile.close()
+print 'XML data saved to %s' % options.xmlfile
+
